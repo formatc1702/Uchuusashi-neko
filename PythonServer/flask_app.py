83cb2a0f4d56adb1786676a0a2b0b640bb4e2d55
Daniel Rojas <github@danielrojas.net>
2017-03-16 20:25:18 +0100

    Clean up Flask app

--- PythonServer/flask_app.py
+++ PythonServer/flask_app.py
@@ -10,9 +10,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask! YEAH'
+    return 'The Flask server is running!'
 
-@app.route('/alt_az/<tle0>/<tle1>/<tle2>/<location>')
+@app.route('/uchuu/alt_az/<tle0>/<tle1>/<tle2>/<location>')
 def alt_az(tle0, tle1, tle2, location):
     alt, az = spacestuff.get_alt_az_from_tle((tle0, tle1, tle2), location)
     alt = round(degrees(alt), 2)
@@ -21,11 +21,12 @@ def alt_az(tle0, tle1, tle2, location):
     # response = 'LAT:\t{}\nLON:\t{}\nALT:\t{}\nAZ: \t{}\n\nRISE: {}\tAZ:  {}\nMAX:  {}\tALT: {}\nSET:  {}\tAZ:  {}\n'.format(iss.sublat, iss.sublong, iss.alt, iss.az, rtx, ra, mtx, ma, stx, sa)
     return response
 
-@app.route('/next_pass/<tle0>/<tle1>/<tle2>/<location>')
+@app.route('/uchuu/next_pass/<tle0>/<tle1>/<tle2>/<location>')
 def next_pass(tle0, tle1, tle2, location):
     import datetime
     rise_time, rise_az, max_time, max_alt, set_time, set_az = spacestuff.get_next_pass_from_tle((tle0, tle1, tle2), location)
     # timezone  correction
+    # TODO: do it properly
     # to compare to http://www.heavens-above.com/PassSummary.aspx?satid=25544
     rise_time_local = rise_time + datetime.timedelta(0,3600)
     max_time_local  = max_time  + datetime.timedelta(0,3600)
@@ -35,27 +36,3 @@ def next_pass(tle0, tle1, tle2, location):
     set_az  = round(degrees(set_az),  2)
     response = 'RISE: {}\tAZ:  {}\nMAX:  {}\tALT: {}\nSET:  {}\tAZ:  {}\n'.format(rise_time_local, rise_az, max_time_local, max_alt, set_time_local, set_az)
     return response
-
-
-@app.route('/alt_az_pred/<line0>/<line1>/<line2>/<location>')
-def alt_az_pred(line0,line1,line2,location):
-    import ephem
-    import datetime
-    from math import pi
-    loc = ephem.city(location)
-    iss = ephem.readtle(line0,line1,line2)
-    response = ''
-    for i in range(0,60):
-        loc.date = datetime.datetime.utcnow() + datetime.timedelta(0, i * 60)
-        iss.compute(loc)
-        response = response + 'LAT:\t{}\tLON:\t{}\tALT:\t{}\tAZ: \t{}\n'.format(iss.sublat,iss.sublong,iss.alt,iss.az)
-    return response
-
-@app.route('/fake.htm')
-def fake():
-    return '<b>FAT</b> FAIL.'
-
-@app.route('/user/<username>/<number>')
-def show_user_profile(username,number):
-    # show the user profile for that user
-    return 'User {} counts like this: {}'.format(username,number)
