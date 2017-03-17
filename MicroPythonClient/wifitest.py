2324912e32c0f271d7f676cdc6b5880cbfc702e4
Daniel Rojas <github@danielrojas.net>
2017-03-17 13:34:51 +0100

    Clean up wifitest.py

--- MicroPythonClient/wifitest.py
+++ MicroPythonClient/wifitest.py
@@ -8,11 +8,9 @@ wifi_config = open('wificonfig.txt','r')
 my_ap = wifi_config.readline().rstrip()
 my_pw = wifi_config.readline().rstrip()
 wifi_config.close()
-print('Hello!')
+
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
-print('SSID: ', my_ap)
-print('PWD:  ', my_pw)
 
 wlan.connect(my_ap, my_pw)
 for i in range(0,40): # attempt to connect
